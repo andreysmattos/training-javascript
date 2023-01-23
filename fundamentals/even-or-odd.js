8200cdae5cb1ce50f0919c452d85c19f9eb83d72
Andrey Mattos <andreysmattos@hotmail.com>
2023-01-23 20:49:33 -0300

    convert-a-number-to-a-string and even-or-odd

---
+++ fundamentals/even-or-odd.js
@@ -0,0 +1,7 @@
+/**
+ * Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+ */
+
+function evenOrOdd(number) {
+  return number % 2 === 0 ? "even" : "Odd";
+}
