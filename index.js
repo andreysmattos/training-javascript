c4f8c4418649f47b4de12c37ad23e2dc6ff95440
Andrey Mattos <andreysmattos@hotmail.com>
2023-01-25 07:30:20 -0300

    set alarm

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-function betterThanAverage(classPoints, yourPoints) {
-  classPoints.push(yourPoints);
-
-  return classPoints.reduce((acc, cur) => acc + cur) / classPoints.length < yourPoints;
+function setAlarm(employed, vacation){
+  return employed && !vacation
 }
-
-console.log(betterThanAverage([100, 40, 34, 57, 29, 72, 57, 88], 5));
-// console.log(past(0, 1, 1));
-// console.log(past(1, 0, 0));
