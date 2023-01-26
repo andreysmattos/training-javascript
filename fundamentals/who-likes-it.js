27b2dc50c8237c6eaac31f3c8e42906047f520cc
Andrey Mattos <andreysmattos@hotmail.com>
2023-01-26 07:27:31 -0300

    disemvowel-trolls and who-likes-it

--- fundamentals/who-likes-it.js
+++ fundamentals/who-likes-it.js
@@ -16,5 +16,12 @@
  */
 
 function likes(names) {
-  // TODO
+  if (names.length === 0) return "no one likes this";
+  if (names.length === 1) return `${names[0]} likes this`;
+  if (names.length === 2) return `${names[0]} and ${names[1]} like this`;
+  if (names.length === 3) return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+
+  return (
+    names.slice(0, 2).join(", ") + ` and ${names.length - 2} others like this`
+  );
 }
--- index.js
+++ index.js
@@ -1,28 +1,20 @@
-function likes(names) {
-  if (names.length === 0) return "no one likes this";
-  if (names.length === 1) return `${names[0]} likes this`;
+function disemvowel(str) {
+  let vowels = ["a", "e", "i", "o", "u"];
+  vowels = vowels.concat(vowels.map((vowel) => vowel.toUpperCase()));
 
-  if (names.length < 4) {
-    console.log(names.slice(0, names.length - 2));
-    return `${names.slice(0, -2).join(", ")}  ${names
-      .slice(-2)
-      .join(" and ")} like this`.trim();
-  }
+  vowels.map((vowel) => {
+    str = str.replaceAll(vowel, "");
+  });
 
-  return (
-    names.slice(0, 2).join(", ") + ` and ${names.length - 2} others like this`
-  );
+  return str;
 }
 
-// console.log(likes(["Jacob", "Alex"]), "Jacob and Alex like this");
-console.log(likes(["Max", "John", "Mark"]), "Max, John and Mark like this");
-
-// console.log(likes([]), "no one likes this");
-// console.log(likes(["Peter"]), "Peter likes this");
-
-// console.log(
-//   likes(["Alex", "Jacob", "Mark", "Max"]),
-//   "Alex, Jacob and 2 others like this"
-// );
-
-// https://www.codewars.com/kata/5266876b8f4bf2da9b000362/train/javascript
+console.log(
+  disemvowel("This website is for losers LOL!"),
+  "Ths wbst s fr lsrs LL!"
+);
+console.log(
+  disemvowel("No offense but,\nYour writing is among the worst I've ever read"),
+  "N ffns bt,\nYr wrtng s mng th wrst 'v vr rd"
+);
+console.log(disemvowel("What are you, a communist?"), "Wht r y,  cmmnst?");
