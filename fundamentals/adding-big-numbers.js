01edd62670f0d39566091763ed4938bac0b7481e
Andrey Mattos <andreysmattos@hotmail.com>
2023-02-01 20:51:41 -0300

    progress

--- fundamentals/adding-big-numbers.js
+++ fundamentals/adding-big-numbers.js
@@ -1,3 +1,5 @@
+// https://www.codewars.com/kata/525f4206b73515bffb000b21
+
 /**
  * We need to sum big numbers and we require your help.
  *
@@ -16,53 +18,50 @@
  */
 
 function add(a, b) {
-  // const MAX_ITEM_CHUNK = 5
-  const result = "";
+  const result = [];
 
-  let chunkA = a.match(/.{1,5}/g);
-  let chunkB = b.match(/.{1,5}/g);
+  let chunkA = a.match(/.{1,2}/g);
+  let chunkB = b.match(/.{1,2}/g);
 
   chunkA = chunkA.map(Number).reverse();
   chunkB = chunkB.map(Number).reverse();
 
-  const smallestChunk = Math.min(chunkA.length, chunkB.length);
+  const maxSize = Math.max(chunkA.length, chunkB.length);
 
-  console.log(chunkA);
-  console.log(chunkB);
+  for (let index = 0; index < maxSize; index++) {
+    console.log("xd");
+    chunkA[index] = chunkA[index] ? chunkA[index] : 0;
+    chunkB[index] = chunkB[index] ? chunkB[index] : 0;
+  }
+
+  // console.log({ chunkA, chunkB });
 
   let rest = 0;
-
-  for (let i = 0; i < smallestChunk; i++) {
+  for (let i = 0; i < maxSize; i++) {
     const sum = chunkA[i] + chunkB[i] + rest;
     let sumString = "" + sum;
 
-    if (sumString.length > 5) {
+    if (sumString.length > 2) {
       rest = Number(sumString.charAt(0));
-      // 123456
-      sumString = sumString.slice(1, 5);
+      sumString = sumString.slice(1, 3);
     } else {
       rest = 0;
     }
-
-    console.log({ sumString });
+    result.push(sumString);
   }
 
-  /*
-  const x = 6789;
-  const y = 6556;
-  // 13345
+  if (rest > 0) {
+    result[result.length - 1] = Number(result[0]) + rest + "";
+  }
 
-  // 67 + 65 = 132
-  // 89 + 56 = 145
+  // console.log({ result, rest });
 
-  console.log("x + y", x + y);
-
-  //   console.log((845 + ))
-
-  // return (BigInt(a) + BigInt(b)).toString();
-  */
+  return result.reverse().join("");
 }
 
-add("289347293874", "90938498237058");
+// add("28934", "909384982");
+//909413916
+console.log(add("888", "222"));
+// , "1110");
 
 // console.log(add('63829983432984289347293874', '90938498237058927340892374089'), "91002328220491911630239667963")
