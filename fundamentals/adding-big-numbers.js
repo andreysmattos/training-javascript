52543651b316aae0948519bcc69b2ad95c6d7e27
Andrey Mattos <andreysmattos@hotmail.com>
2023-02-02 07:34:23 -0300

    feat() finish adding-big-numbers

--- fundamentals/adding-big-numbers.js
+++ fundamentals/adding-big-numbers.js
@@ -1,5 +1,3 @@
-// https://www.codewars.com/kata/525f4206b73515bffb000b21
-
 /**
  * We need to sum big numbers and we require your help.
  *
@@ -20,48 +18,51 @@
 function add(a, b) {
   const result = [];
 
-  let chunkA = a.match(/.{1,2}/g);
-  let chunkB = b.match(/.{1,2}/g);
+  const chunkA = a.split("").reverse().map(Number);
+  const chunkB = b.split("").reverse().map(Number);
 
-  chunkA = chunkA.map(Number).reverse();
-  chunkB = chunkB.map(Number).reverse();
+  console.log({ chunkA, chunkB });
 
   const maxSize = Math.max(chunkA.length, chunkB.length);
 
   for (let index = 0; index < maxSize; index++) {
-    console.log("xd");
     chunkA[index] = chunkA[index] ? chunkA[index] : 0;
     chunkB[index] = chunkB[index] ? chunkB[index] : 0;
   }
 
-  // console.log({ chunkA, chunkB });
+  console.log({ chunkA, chunkB });
 
   let rest = 0;
   for (let i = 0; i < maxSize; i++) {
     const sum = chunkA[i] + chunkB[i] + rest;
     let sumString = "" + sum;
 
-    if (sumString.length > 2) {
+    if (sumString.length > 1) {
       rest = Number(sumString.charAt(0));
-      sumString = sumString.slice(1, 3);
+      sumString = sumString.slice(1, 2);
     } else {
       rest = 0;
     }
     result.push(sumString);
   }
 
-  if (rest > 0) {
-    result[result.length - 1] = Number(result[0]) + rest + "";
-  }
-
-  // console.log({ result, rest });
+  if (rest > 0) result.push("" + rest);
 
   return result.reverse().join("");
 }
 
-// add("28934", "909384982");
-//909413916
-console.log(add("888", "222"));
-// , "1110");
+// Alternative
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Bitwise_NOT
 
-// console.log(add('63829983432984289347293874', '90938498237058927340892374089'), "91002328220491911630239667963")
+function add(a, b) {
+  var res = "",
+    c = 0;
+  a = a.split("");
+  b = b.split("");
+  while (a.length || b.length || c) {
+    c += ~~a.pop() + ~~b.pop();
+    res = (c % 10) + res;
+    c = c > 9;
+  }
+  return res;
+}
