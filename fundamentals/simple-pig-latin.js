4e516470db000a0fb619a6151c712aa0ff2680bc
Andrey Mattos <andreysmattos@hotmail.com>
2023-02-08 20:47:02 -0300

    moving-zeros-to-the-end

--- fundamentals/simple-pig-latin.js
+++ fundamentals/simple-pig-latin.js
@@ -21,6 +21,11 @@ function pigIt(str) {
   return wordChanged.join(" ");
 }
 
+// Alternative:
+function pigIt(str) {
+  return str.replace(/(\w)(\w*)(\s|$)/g, "$2$1ay$3");
+}
+
 console.log(pigIt("Pig latin is cool")); // igPay atinlay siay oolcay
 console.log(pigIt("This is my string")); // hisTay siay ymay tringsay
 console.log(pigIt("Hello world !")); // elloHay orldway !
